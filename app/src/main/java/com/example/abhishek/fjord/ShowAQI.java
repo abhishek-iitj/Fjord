435776a3cc58b4a863e940b1bd9894de9a863d27
Abhishek Sah <abhi.sah.97@gmail.com>
2018-10-28 02:13:20 +0530

    [Abhishek] Extracts lat/long from received intent extras

--- app/src/main/java/com/example/abhishek/fjord/ShowAQI.java
+++ app/src/main/java/com/example/abhishek/fjord/ShowAQI.java
@@ -2,12 +2,18 @@ package com.example.abhishek.fjord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ShowAQI extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_aqi);
+        Bundle extras = getIntent().getExtras();
+        String latitude, longitude;
+        latitude = extras.getString("lat");
+        longitude = extras.getString("long");
+        Log.d("Latitude", latitude);
+        Log.d("Longitude", longitude);
     }
 }
