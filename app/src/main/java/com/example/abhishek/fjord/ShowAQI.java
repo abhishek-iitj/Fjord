294e21734c3cce9bf72aa99a2d150adbfc144d09
Abhishek Sah <abhi.sah.97@gmail.com>
2018-10-28 01:09:13 +0530

    [Abhishek] Adds new activity to show air quality data

---
+++ app/src/main/java/com/example/abhishek/fjord/ShowAQI.java
@@ -0,0 +1,13 @@
+package com.example.abhishek.fjord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowAQI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_aqi);
+    }
+}
